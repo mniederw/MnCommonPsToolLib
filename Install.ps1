97f87a712596e5fcced25e30a72b402f0ced2937
mniederw <marc.niederwieser@gmx.net>
2019-01-07 18:45:40 +0100

    improve installation

--- Install.ps1
+++ Install.ps1
@@ -12,11 +12,12 @@ function OutProgress                          ( [String] $line ){ Write-Host -Fo
 function OutProgressText                      ( [String] $line ){ Write-Host -ForegroundColor DarkGray -NoNewLine $line; }
 function OutQuestion                          ( [String] $line ){ Write-Host -ForegroundColor Cyan     -NoNewline $line; }
 function FsEntryMakeTrailingBackslash         ( [String] $fsEntry ){ [String] $result = $fsEntry; if( -not $result.EndsWith("\") ){ $result += "\"; } return [String] $result; }
+function FsEntryRemoveTrailingBackslash       ( [String] $fsEntry ){ [String] $result = $fsEntry; if( $result -ne "" ){ while( $result.EndsWith("\") ){ $result = $result.Remove($result.Length-1); } if( $result -eq "" ){ $result = $fsEntry; } } return [String] $result; } # leading backslashes are not removed.
 function FsEntryGetAbsolutePath               ( [String] $fsEntry ){ return [String] ($ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($fsEntry)); }
-function PsModulePathList                     (){ return [String[]] ([Environment]::GetEnvironmentVariable($envVar, "Machine").Split(";",[System.StringSplitOptions]::RemoveEmptyEntries) | ForEach-Object{ FsEntryMakeTrailingBackslash $_; }); }
-function PsModulePathContains                 ( [String] $d ){ return [Boolean] ((PsModulePathList) -contains (FsEntryMakeTrailingBackslash $d)); }
-function PsModulePathAdd                      ( [String] $d ){ PsModulePathSet ((PsModulePathList)+@( (FsEntryMakeTrailingBackslash $d) )); }
-function PsModulePathDel                      ( [String] $d ){ PsModulePathSet ((PsModulePathList) | Where-Object{ $_ -ne (FsEntryMakeTrailingBackslash $d) }); }
+function PsModulePathList                     (){ return [String[]] ([Environment]::GetEnvironmentVariable($envVar, "Machine").Split(";",[System.StringSplitOptions]::RemoveEmptyEntries)); }
+function PsModulePathContains                 ( [String] $d ){ return [Boolean] ((PsModulePathList | ForEach-Object { FsEntryRemoveTrailingBackslash $_ }) -contains (FsEntryRemoveTrailingBackslash $d)); }
+function PsModulePathAdd                      ( [String] $d ){ if( PsModulePathContains $d ){ return; } PsModulePathSet ((PsModulePathList)+@( (FsEntryMakeTrailingBackslash $d) )); }
+function PsModulePathDel                      ( [String] $d ){ PsModulePathSet (PsModulePathList | Where-Object{ (FsEntryRemoveTrailingBackslash $_) -ne (FsEntryRemoveTrailingBackslash $d) }); }
 function PsModulePathSet                      ( [String[]] $a ){ [Environment]::SetEnvironmentVariable($envVar, ($a -join ";"), "Machine"); }
 function DirExists                            ( [String] $dir ){ try{ return [Boolean] (Test-Path -PathType Container -LiteralPath $dir ); }catch{ throw [Exception] "DirExists($dir) failed because $($_.Exception.Message)"; } }
 function DirListDirs                          ( [String] $d ){ return [String[]] (@()+(Get-ChildItem -Force -Directory -Path $d | ForEach-Object{ $_.FullName })); }
@@ -35,7 +36,10 @@ function InstallSrcPathToPsModulePathIfNotInst( [String] $srcDir ){ OutProgress 
                                                 if( (PsModulePathContains $srcDir) ){ OutProgress "Already installed so environment variable not changed."; }else{ ProcessRestartInElevatedAdminMode; PsModulePathAdd $srcDir; } }
 function OutCurrentInstallState               ( [String] $srcRootDir, [String] $moduleTarDir, [String] $color = "White" ){ [Boolean] $srcRootDirIsInPath = PsModulePathContains $srcRootDir; [Boolean] $moduleTarDirExists = DirExists $moduleTarDir; 
                                                 [String] $installedText = switch($srcRootDirIsInPath){ ($true){"Installed-for-Developers. "} default{switch($moduleTarDirExists){ ($true){"Installed-in-Standard-Mode. "} default{"Not-Installed. "}}}}; 
-                                                OutProgressText "Current installation state: "; Write-Host -ForegroundColor $color $installedText; }
+                                                OutProgressText "Current installation mode: "; Write-Host -ForegroundColor $color $installedText; }
+function SelfUpdate                           (){ $PSModuleAutoLoadingPreference = "All";
+                                                try{ Import-Module "MnCommonPsToolLib.psm1"; MnCommonPsToolLib\MnCommonPsToolLibSelfUpdate; }
+                                                catch{ OutProgress "Please restart shell and maybe calling file manager and retry"; throw; } }
 
 
 [String] $tarRootDir = "$Env:ProgramW6432\WindowsPowerShell\Modules"; # more see: https://msdn.microsoft.com/en-us/library/dd878350(v=vs.85).aspx
@@ -47,7 +51,7 @@ if( $dirsWithPsm1Files.Count -ne 1 ){ throw [Exception] "Tool is designed for wo
 [String] $moduleTarDir = "$tarRootDir\$moduleName";
 [Boolean] $isDev = DirExists "$srcRootDir\.git";
 OutInfo         "Install Menu for Powershell Module - $moduleName";
-OutInfo         "-------------------------------------------------------------------------------`n";
+OutInfo         "-------------------------------------$("-"*($moduleName.Length))`n";
 OutProgress     "  For installation or uninstallation the elevated administrator mode is ";
 OutProgress     "  required and this tool automatically prompts for it when nessessary. ";
 OutProgress     "  Powershell requires for any installation of a module that its file must be ";
@@ -55,38 +59,38 @@ OutProgress     "  located in a folder with the same name as the module name. ";
 OutProgress     "  An installation in standard mode does first an uninstallation and then for ";
 OutProgress     "  installation it copies the ps module folder to the common ps module folder ";
 OutProgress     "  for all users. An alternative installation for developers does also first an ";
-OutProgress     "  uninstallation and then it adds the path of the module folder as entry to the ";
-OutProgress     "  ps module path environment variable ($envVar). ";
+OutProgress     "  uninstallation and then it adds the path of the module folder as entry to ";
+OutProgress     "  the ps module path environment variable ($envVar). ";
 OutProgress     "  An uninstallation does both, it removes the copied folder from the common ps ";
 OutProgress     "  module folder for all users and it removes the path entry from the ps module ";
 OutProgress     "  path environment variable. ";
-OutProgress     "  (*) If you just switched install mode, before using these commands you probably ";
-OutProgress     "      need to restart your calling shell or calling program (ex: a file manager) ";
-OutProgress     "      because it still references the previous installed module. ";
+OutProgress     "  Imporant note: After any installation the current running programs which are ";
+OutProgress     "  using the old PsModulePath or which did load previously the old module, they ";
+OutProgress     "  need to be restarted before they can use new installed module. This usually ";
+OutProgress     "  applies for a file manager or powershell sessions, but not for win-explorer. ";
 OutProgress     "  By using this software you agree with the terms of GPL3. ";
 OutProgress     "  ";
 OutProgress     "  Current environment:";
 OutProgress     "    IsInElevatedAdminMode = $(ProcessIsRunningInElevatedAdminMode).";
-OutProgress     "    SrcRootDir = '$srcRootDir' ";
-OutProgress     "    CommonPsModuleFolderForAllUsers = '$tarRootDir' ";
-OutProgressText "    "; OutCurrentInstallState $srcRootDir $moduleTarDir "White";
+OutProgress     "    SrcRootDir = '$srcRootDir'. ";
+OutProgress     "    PsModulePath contains SrcRootDir = $(PsModulePathContains $srcRootDir). ";
+OutProgress     "    PsModuleFolder for All Users = '$tarRootDir'. ";
+OutProgressText "    "; OutCurrentInstallState $srcRootDir $moduleTarDir $(switch($sel -ne ""){($true){"DarkGray"}default{"White"}});
 OutInfo         "";
 OutInfo         "  I = Install or reinstall in standard mode. ";
 OutInfo         "  A = Alternative installation for developers which uses module at current location to change and test the module. ";
 OutInfo         "  N = Uninstall. ";
-OutInfo         "  U = When installed (*) in standard mode do update from web. ";
-if( $isDev ){ OutInfo "  H = For developer and when installed (*): actualize sha2 hash file of library. "; }
+OutInfo         "  U = When installed in standard mode do update from web. "; # in future do download and also switch to standard mode.
 OutInfo         "  Q = Quit. `n";
 if( $sel -ne "" ){ OutProgress "Selection: $sel "; }
-while( @("I","A","N","U","Q","H") -notcontains $sel ){
+while( @("I","A","N","U","Q") -notcontains $sel ){
   OutQuestion "Enter selection case insensitive and press enter: ";
   $sel = (Read-Host);
 }
 $Global:ArgsForRestartInElevatedAdminMode = $sel; 
-if( $sel -eq "N"             ){ UninstallDir $moduleTarDir; UninstallSrcPath $srcRootDir;                                       OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
-if( $sel -eq "I"             ){ UninstallDir $moduleTarDir; UninstallSrcPath $srcRootDir; InstallDir $moduleSrcDir $tarRootDir; OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
-if( $sel -eq "A"             ){ UninstallDir $moduleTarDir; InstallSrcPathToPsModulePathIfNotInst $srcRootDir;                  OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
-if( $sel -eq "U"             ){ $PSModuleAutoLoadingPreference = "All"; try{ Import-Module "MnCommonPsToolLib.psm1"; MnCommonPsToolLib\MnCommonPsToolLibSelfUpdate;                                            }catch{ OutProgress "Please restart shell and maybe calling file manager and retry"; throw; } }
-if( $sel -eq "H" -and $isDev ){ $PSModuleAutoLoadingPreference = "All"; try{ Import-Module "MnCommonPsToolLib.psm1"; MnCommonPsToolLib\FileUpdateItsHashSha2FileIfNessessary "$moduleSrcDir\$moduleName.psm1"; }catch{ OutProgress "Please restart shell and maybe calling file manager and retry"; throw; } }
-if( $sel -eq "Q"             ){ OutProgress "Quit."; }
+if( $sel -eq "N" ){ UninstallDir $moduleTarDir; UninstallSrcPath $srcRootDir;                                       OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
+if( $sel -eq "I" ){ UninstallDir $moduleTarDir; UninstallSrcPath $srcRootDir; InstallDir $moduleSrcDir $tarRootDir; OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
+if( $sel -eq "A" ){ UninstallDir $moduleTarDir; InstallSrcPathToPsModulePathIfNotInst $srcRootDir;                  OutCurrentInstallState $srcRootDir $moduleTarDir "Green"; }
+if( $sel -eq "U" ){ SelfUpdate; }
+if( $sel -eq "Q" ){ OutProgress "Quit."; }
 OutQuestion "Finished. Press enter to exit. "; Read-Host;
