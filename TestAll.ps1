a5b1f365d3d23c9d9912c7ded3effa1082da8b5c
Marc Niederwieser <marc.niederwieser@gmx.net>
2022-08-09 03:10:08 +0200

    fixed modulepath for linux

--- TestAll.ps1
+++ TestAll.ps1
@@ -2,6 +2,11 @@
 
 Write-Output "Test all - run all examples, the script analyser and the unit tests with pwsh (powershell)";
 
+Write-Output "Show Platform   = $([System.Environment]::OSVersion.Platform)"; # "Win32NT" or "Unix"
+Write-Output "Show OS         = $($PSVersionTable.OS)"; # "Microsoft Windows 10.0.19042" or ...
+Write-Output "Show IsWindows  = $($IsWindows)";
+Write-Output "Show IsLinux    = $($IsLinux)";
+Write-Output "Show IsMacOS    = $($IsMacOS)";
 Write-Output "Show CurrentDir = `"$PWD`"";
   # Local-Win10   : "D:\mywork\MyOwner\MyRepo"
   # Github-Windows: "D:\a\MyRepo\MyRepo"
@@ -13,7 +18,7 @@ Write-Output "Show GITHUB_WORKSPACE = `"$($env:GITHUB_WORKSPACE)`"";
   # Github-ubuntu : "/home/runner/work/MnCommonPsToolLib/MnCommonPsToolLib"
 
 Write-Output "Show PSModulePath = `"`"";
-$env:PSModulePath -split ";" | Where-Object{ $null -ne $_ } | ForEach-Object{ Write-Output "Show PSModulePath += `";$_`""; }
+$($env:PSModulePath).Split(";:") | Where-Object{ $null -ne $_ } | ForEach-Object{ Write-Output "Show PSModulePath += `";$_`""; }
   # Local-Win10   : ";C:\Users\myuser\Documents\WindowsPowerShell\Modules"
   #                 ";C:\Program Files (x86)\WindowsPowerShell\Modules"
   #                 ";C:\Windows\system32\WindowsPowerShell\v1.0\Modules"
@@ -31,10 +36,13 @@ $env:PSModulePath -split ";" | Where-Object{ $null -ne $_ } | ForEach-Object{ Wr
   #                 ";C:\Windows\system32\WindowsPowerShell\v1.0\Modules"
   #                 ";C:\Program Files\Microsoft SQL Server\130\Tools\PowerShell\Modules\"
   #                 ";C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\platform\PowerShell"
-  # Github-ubuntu : ":/home/runner/.local/share/powershell/Modules"
+  # Local-Ubuntu  : ":/home/nn/.local/share/powershell/Modules"
   #                 ":/usr/local/share/powershell/Modules"
   #                 ":/opt/microsoft/powershell/7/Modules"
-
+# Github-ubuntu : ":/home/runner/.local/share/powershell/Modules"
+  #                 ":/usr/local/share/powershell/Modules"
+  #                 ":/opt/microsoft/powershell/7/Modules"
+  
 Write-Output "Set mode to stop on errors.";
 $Global:ErrorActionPreference = "Stop"; 
 trap [Exception] { $Host.UI.WriteErrorLine("Trap: $_"); Read-Host; break; }
@@ -42,8 +50,9 @@ trap [Exception] { $Host.UI.WriteErrorLine("Trap: $_"); Read-Host; break; }
 Write-Output "Set disable autoloading modules.";
 $PSModuleAutoLoadingPreference = "none";
 
-Write-Output "Set extend PSModulePath by current dir.";
-[Environment]::SetEnvironmentVariable("PSModulePath","${env:PSModulePath};$PWD","Process"); # add ps module to path
+Write-Output "Extend PSModulePath by current dir.";
+[String] $pathsep = ":"; if( $IsWindows ){ $pathsep = ";"; }
+[Environment]::SetEnvironmentVariable("PSModulePath","${env:PSModulePath}$pathsep$PWD","Process"); # add ps module to path
 
 Write-Output "Load library";
 Import-Module "MnCommonPsToolLib.psm1";
