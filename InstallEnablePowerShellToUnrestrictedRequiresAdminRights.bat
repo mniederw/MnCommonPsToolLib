4ec1ab0306f0d4b0ad56f599ea2361ae76927c76
Marc Niederwieser <marc.niederwieser@gmx.net>
2023-02-01 01:12:30 +0100

    V7.11

--- InstallEnablePowerShellToUnrestrictedRequiresAdminRights.bat
+++ InstallEnablePowerShellToUnrestrictedRequiresAdminRights.bat
@@ -1,50 +1,59 @@
 @ECHO off
 
-SET mode=Bypass
-
-ECHO Enable execution mode %mode% for powershell scripts on this machine on 64bit and 32bit environment.
+ECHO Enable execution mode Bypass for powershell scripts on this machine on PS7 or 64bit and 32bit powershell environment.
 ECHO.
-ECHO Current mode on 64bit environment is: & %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe Get-Executionpolicy
-ECHO Current mode on 32bit environment is: & %SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe Get-Executionpolicy
-ECHO Current modes by current ps environment: & PowerShell.exe -NoProfile -Command Get-ExecutionPolicy -List
+ECHO Usually you set mode to Bypass if you trust yourself, that you won't click by accident on unknown ps script files.
+ECHO.
+ECHO Current mode on ps7       environment: 
+  IF     EXIST "%ProgramFiles%\PowerShell\7\pwsh.EXE" ( "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Get-Executionpolicy ) ELSE ( ECHO pwsh not found )
+ECHO Current mode on ps5-64bit environment: 
+  %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -Command Get-Executionpolicy
+ECHO Current mode on ps5-32bit environment:
+  %SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe -Command Get-Executionpolicy
 ECHO.
 ECHO Available modes:
-ECHO   Restricted   - No scripts can be run. Windows PowerShell can be used only in interactive mode. Default after os installation.
-ECHO   AllSigned    - Only scripts signed by a trusted publisher can be run.
-ECHO   RemoteSigned - Downloaded scripts must be signed by a trusted publisher before they can be run.
-ECHO   Unrestricted - No restrictions; all Windows PowerShell scripts can be run, but with security warning if running from a share.
-ECHO   Bypass       - As Unrestricted but without the warning.
+ECHO   Restricted    - No ps scripts can be run, ps can be used only in interactive mode. Default after OS installation.
+ECHO   AllSigned     - Only ps scripts signed by a trusted publisher can be run.
+ECHO   RemoteSigned  - Downloaded ps scripts must be signed by a trusted publisher before they can be run.
+ECHO   Unrestricted  - No restrictions; all ps scripts can be run, but with security warning if running from a share.
+ECHO   Bypass        - As Unrestricted but without the warning.
 ECHO.
 ECHO Existing scopes in precedence order:
-ECHO   MachinePolicy - represents a Group Policy in effect at the Computer level, generally applied only in a domain, but can be done locally as well.
-ECHO   UserPolicy    - represents a Group Policy in effect on the user, is also typically only used in enterprise environments.
-ECHO   Process       - specific to this instance of PowerShell, changes to the policy in this scope will not affect other running PowerShell processes,
-ECHO                   and will be ineffective after this session is terminated. This can be configured by the -ExecutionPolicy parameter
-ECHO                   when PowerShell is launched, or it can be set with the proper Set-ExecutionPolicy syntax from within the session.
-ECHO   CurrentUser   - is configured in the local registry and applies to the user account used to launch PowerShell, can be modified with Set-ExecutionPolicy.
-ECHO   LocalMachine -  is configured in the local registry and applying to all users on the system, is the default scope that is changed if Set-ExecutionPolicy
-ECHO                   is run without the -Scope parameter. As it applies to all users on the system, it can only be changed from an elevated session.
+ECHO   MachinePolicy - Group policy in effect at the computer level, generally applied only in a domain, but can be used locally as well.
+ECHO   UserPolicy    - Group policy in effect on the user, is also typically only used in enterprise environments.
+ECHO   Process       - Value of current ps instance, changes affects no other ps processes, can be set on launch with -ExecutionPolicy param.
+ECHO   CurrentUser   - Is configured in the local registry and applies to the user account used to launch ps.
+ECHO   LocalMachine  - Is configured in the local registry and applying to all users on the system.
 ECHO.
 ECHO Note: In any mode a script can always be run with:
-ECHO   PowerShell.exe -ExecutionPolicy Unrestricted -NoProfile -File "myfile.ps1"
-ECHO Get more info with: powershell.exe Get-Help Set-ExecutionPolicy -full
-ECHO   or see: https://technet.microsoft.com/en-us/library/dd347641
-ECHO If execution mode has not a required level then you can get the following warning as example:
+ECHO                            powershell.exe -ExecutionPolicy Unrestricted -NoProfile -File "myfile.ps1"
+ECHO Get more info with:        powershell.exe Get-Help Set-ExecutionPolicy -full
+ECHO Get current config with:   powershell.exe -NoProfile -Command Get-ExecutionPolicy -List
+ECHO If mode has not a required level then you will get the following warning as example:
 ECHO   Security Warning: Run only scripts that you trust. While scripts from the Internet can be useful, this script can potentially
 ECHO   harm your computer. Do you want to run [D] Do not run  [R] Run once  [S] Suspend [?] Help (default is "D"):
-ECHO Usually you set mode to Bypass if you trust yourself, that you won't click by accident on unknown powershell script files.
+ECHO.
+ECHO Modes can be reconfigured by continue below or by calling within ps the command (default scope is LocalMachine): Set-ExecutionPolicy
 ECHO.
 
-SET /p answer=Are you sure to enable powershell execution mode %mode% and did you run this batch as administrator [y/n]?
+SET /p answer=Are you sure to enable powershell execution mode Bypass and did you run this batch as administrator [y/n]? 
 IF /I "%answer%" NEQ "y" ( ECHO Aborted & PAUSE & EXIT /B 1 )
 ECHO.
 
-SET cmd=%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe Set-Executionpolicy %mode%
-ECHO Enable for 64bit: %cmd% & %cmd%
-IF %ERRORLEVEL% neq 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /B 1 )
+IF EXIST "%ProgramFiles%\PowerShell\7\pwsh.EXE" (
+  ECHO Enable Bypass for ps7      : "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
+                                    "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
+                                    IF %ERRORLEVEL% NEQ 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /B 1 )
+)
 
-SET cmd=%SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe Set-Executionpolicy %mode%
-ECHO Enable for 32bit: %cmd% & %cmd%
-IF %ERRORLEVEL% neq 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /b 1 )
+  ECHO Enable Bypass for ps5-64bit: "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" -Command Set-Executionpolicy Bypass
+                                    "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" -Command Set-Executionpolicy Bypass
+                                    IF %ERRORLEVEL% NEQ 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /B 1 )
 
-ECHO. & ECHO OK, done. Press enter to exit. & PAUSE > :NUL
+                                    ECHO Enable Bypass for ps5-32bit: "%SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe" -Command Set-Executionpolicy Bypass
+                                    "%SystemRoot%\syswow64\WindowsPowerShell\v1.0\powershell.exe" -Command Set-Executionpolicy Bypass
+                                    IF %ERRORLEVEL% NEQ 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /b 1 )
+
+ECHO.
+ECHO OK, done. Press enter to exit.
+PAUSE
