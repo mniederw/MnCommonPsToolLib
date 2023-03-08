ad8502fc933313f05eea4c3602fb9d5d0f3cc311
Marc Niederwieser <marc.niederwieser@gmx.net>
2023-03-08 02:22:32 +0100

    V7.16

--- MnCommonPsToolLib/MnCommonPsToolLib.psm1
+++ MnCommonPsToolLib/MnCommonPsToolLib.psm1
@@ -650,7 +650,7 @@ function ProcessListInstalledAppx             (){ if( ! (OsIsWindows) ){ return 
                                                     Import-Module -Name Appx -UseWindowsPowerShell 3> $null;
                                                       # We suppress the output: WARNING: Module Appx is loaded in Windows PowerShell using WinPSCompatSession remoting session; 
                                                       #   please note that all input and output of commands from this module will be deserialized objects. 
-                                                      #  If you want to load this module into PowerShell please use 'Import-Module -SkipEditionCheck' syntax.
+                                                      #   If you want to load this module into PowerShell please use 'Import-Module -SkipEditionCheck' syntax.
                                                   }
                                                   return [String[]] (@()+(Get-AppxPackage | Where-Object{$null -ne $_} |
                                                     ForEach-Object{ "$($_.PackageFullName)" } | Sort)); }
