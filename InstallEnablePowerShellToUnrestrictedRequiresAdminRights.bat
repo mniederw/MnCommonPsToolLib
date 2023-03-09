22dde973835d3bb3df2db5c507a574df40ba2add
Marc Niederwieser <marc.niederwieser@gmx.net>
2023-03-09 12:30:22 +0100

    V7.17

--- InstallEnablePowerShellToUnrestrictedRequiresAdminRights.bat
+++ InstallEnablePowerShellToUnrestrictedRequiresAdminRights.bat
@@ -5,7 +5,7 @@ ECHO.
 ECHO Usually you set mode to Bypass if you trust yourself, that you won't click by accident on unknown ps script files.
 ECHO.
 ECHO Current mode on ps7       environment: 
-  IF     EXIST "%ProgramFiles%\PowerShell\7\pwsh.EXE" ( "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Get-Executionpolicy ) ELSE ( ECHO pwsh not found )
+  IF     EXIST "%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE" ( "%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE" -Command Get-Executionpolicy ) ELSE ( ECHO "'%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE' not found" )
 ECHO Current mode on ps5-64bit environment: 
   %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command Get-Executionpolicy
 ECHO Current mode on ps5-32bit environment:
@@ -40,9 +40,9 @@ SET /p answer=Are you sure to enable powershell execution mode Bypass and did yo
 IF /I "%answer%" NEQ "y" ( ECHO Aborted & PAUSE & EXIT /B 1 )
 ECHO.
 
-IF EXIST "%ProgramFiles%\PowerShell\7\pwsh.EXE" (
-  ECHO Enable Bypass for ps7      : "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
-                                    "%ProgramFiles%\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
+IF EXIST "%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE" (
+  ECHO Enable Bypass for ps7      : "%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
+                                    "%SystemDrive%\Program Files\PowerShell\7\pwsh.EXE" -Command Set-Executionpolicy Bypass
                                     IF %ERRORLEVEL% NEQ 0 ( ECHO Error: rc=%ERRORLEVEL% & PAUSE & EXIT /B 1 )
 )
 
