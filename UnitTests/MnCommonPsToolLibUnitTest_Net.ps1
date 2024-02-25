3558e09415bca31c3b259055797690b4a75caa3f
Marc Niederwieser <marc.niederwieser@gmx.net>
2024-02-25 19:03:46 +0100

    V7.45

--- UnitTests/MnCommonPsToolLibUnitTest_Net.ps1
+++ UnitTests/MnCommonPsToolLibUnitTest_Net.ps1
@@ -21,14 +21,15 @@ function Test_Net(){
   #   NetWebRequestLastModifiedFailSafe    ( [String] $url ){ # Requests metadata from a downloadable file. Return DateTime.MaxValue in case of any problem
   #
   [String] $url = "https://raw.githubusercontent.com/mniederw/MnCommonPsToolLib/main/Readme.txt";
-  [String] $tar = "$env:TEMP/tmp/MnCommonPsToolLib_UnitTest_Net.tmp";
   OutProgress "NetDownloadIsSuccessful"  ; Assert (NetDownloadIsSuccessful $url);
-  OutProgress "NetDownloadFile"          ; NetDownloadFile       $url $tar; Assert ((FileGetSize $tar) -gt 0); FileDelete $tar;
+  [String] $tmp = FileGetTempFile; 
+  OutProgress "NetDownloadFile"          ; NetDownloadFile       $url $tmp; Assert ((FileGetSize $tmp) -gt 0); FileDelete $tmp;
   OutProgress "NetDownloadToString"      ; Assert ((NetDownloadToString $url) -gt 0);
   if( (ProcessFindExecutableInPath "curl") -eq "" ){
     OutProgress "Curl is not in path, so cannot test methods using it.";
   }else{
-    OutProgress "NetDownloadFileByCurl"    ; NetDownloadFileByCurl $url $tar; Assert ((FileGetSize $tar) -gt 0); FileDelete $tar;
+    $tmp = FileGetTempFile;
+    OutProgress "NetDownloadFileByCurl"    ; NetDownloadFileByCurl $url $tmp; Assert ((FileGetSize $tmp) -gt 0); FileDelete $tmp;
     OutProgress "NetDownloadToStringByCurl"; Assert ((NetDownloadToStringByCurl $url) -gt 0);
   }
   #
