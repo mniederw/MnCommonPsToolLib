c102201bccd07d1a5e282077c1026110d98b4559
Marc Niederwieser <marc.niederwieser@gmx.net>
2024-02-25 19:48:05 +0100

    V7.45

--- TestAll.ps1
+++ TestAll.ps1
@@ -96,7 +96,7 @@ $tmp = FileGetTempFile;
 if( OsIsWindows ){
   & "C:\Windows\system32\curl.exe" "--show-error" "--fail" "--output" $tmp "--silent" "--create-dirs" "--connect-timeout" "70" "--retry" "2" "--retry-delay" "5" "--tlsv1.2" "--remote-time" "--location" "--max-redirs" "50" "--stderr" "-" "--user-agent" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0" "--url" "https://raw.githubusercontent.com/mniederw/MnCommonPsToolLib/main/Readme.txt";
 }else{
-  & "/usr/local/opt/curl/bin/curl" "--show-error" "--fail" "--output" $tmp "--silent" "--create-dirs" "--connect-timeout" "70" "--retry" "2" "--retry-delay" "5" "--tlsv1.2" "--remote-time" "--location" "--max-redirs" "50" "--stderr" "-" "--user-agent" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0" "--url" "https://raw.githubusercontent.com/mniederw/MnCommonPsToolLib/main/Readme.txt";
+  & (ProcessFindExecutableInPath "curl") "--show-error" "--fail" "--output" $tmp "--silent" "--create-dirs" "--connect-timeout" "70" "--retry" "2" "--retry-delay" "5" "--tlsv1.2" "--remote-time" "--location" "--max-redirs" "50" "--stderr" "-" "--user-agent" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0" "--url" "https://raw.githubusercontent.com/mniederw/MnCommonPsToolLib/main/Readme.txt";
 }
 FileDelete $tmp;
 OutInfo "Ok, done.";
