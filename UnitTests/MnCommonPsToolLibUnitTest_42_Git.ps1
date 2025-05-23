919a5062ef4c41a396b0f7e1104d74808224dba1
mniederw <marc.niederwieser@gmx.net>
2025-05-23 13:31:29 +0200

    V7.92

--- UnitTests/MnCommonPsToolLibUnitTest_42_Git.ps1
+++ UnitTests/MnCommonPsToolLibUnitTest_42_Git.ps1
@@ -29,7 +29,7 @@ function UnitTest_Git(){
   GitSwitch $repoDir "trunk";
   GitAdd "$repoDir/Readme.txt";
   try{
-    GitMerge $repoDir "main";
+    [Boolean] $hasNoConflicts = GitMerge $repoDir "main";
   }catch{
     # 2024-03 on github action we get: failed with rc=128  Committer identity unknown *** Please tell me who you are.
     #   To set your account's default identity run:  git config --global user.email "you@example.com"; git config --global user.name "Your Name";
